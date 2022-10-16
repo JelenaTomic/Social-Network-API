1e3f351f56d9c102956089add1a6fc4c5bb9ae4f
JelenaTomic <tomic.elena09@gmail.com>
2022-10-15 20:06:36 -0500

    added user.js in models file

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: 'Username is required',
+            unique: true
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            match: [/.+@.+\..+/],
+            required: 'Password is required'
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+  
+
+const User = model('User', UserSchema);
+
+module.exports = User;
